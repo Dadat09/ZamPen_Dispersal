9a889d249bba5a415908e8f0d21f3c36de7fa3d6
REgguria <145839362+REgguria@users.noreply.github.com>
2024-08-24 14:06:09 +0800

    added navbar
    navbar

--- DispersalTracking/DispersalTracking/settings.py
+++ DispersalTracking/DispersalTracking/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'DispersalTracking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+      os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
